3b02f13af427dc71f250465bf74bd6755a7c821f
Tyler Smith <tyler@plantarum.ca>
2018-04-09 15:53:45 -0400

    moved space and backspace to thumb clusters
    moved function keys to escape/butterfly

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -136,18 +136,34 @@ KEYMAPS(
   (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_LeftAlt,   Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_LeftControl, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Backspace,
+   Key_LeftControl, Key_Z, Key_X, Key_C, Key_V, Key_B, ShiftToLayer(FUNCTION),
    // Left thumb cluster:
-   Key_LeftShift, ShiftToLayer(FUNCTION), Key_LeftGui, Key_LeftShift,
+   Key_LeftShift, Key_Backspace, Key_LeftGui, Key_LeftShift,
    ShiftToLayer(FUNCTION), // palm key
 
    Key_Escape,  Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_KeypadNumLock,
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   Key_Spacebar,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
+   ShiftToLayer(FUNCTION),  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
    // Right thumb cluster:
-   Key_RightShift, Key_LeftAlt, ShiftToLayer(FUNCTION), Key_RightShift,
+   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightShift,
    ShiftToLayer(FUNCTION)), // palm key
+
+  
+  [NUMPAD] =  KEYMAP_STACKED
+  (___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___,
+   ___,
+
+   M(MACRO_VERSION_INFO),  ___, Key_Keypad7, Key_Keypad8,   Key_Keypad9,        Key_KeypadSubtract, ___,
+   ___,                    ___, Key_Keypad4, Key_Keypad5,   Key_Keypad6,        Key_KeypadAdd,      ___,
+                           ___, Key_Keypad1, Key_Keypad2,   Key_Keypad3,        Key_Equals,         ___,
+   ___,                    ___, Key_Keypad0, Key_KeypadDot, Key_KeypadMultiply, Key_KeypadDivide,   Key_Enter,
+   ___, ___, ___, ___,
+   ___),
 
   [FUNCTION] =  KEYMAP_STACKED
   (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           XXX,
@@ -162,23 +178,7 @@ KEYMAPS(
    ___, Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,
    Key_PcApplication,          Key_Mute,               Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
    ___, ___, Key_Enter, ___,
-   ___),
-
-  [NUMPAD] =  KEYMAP_STACKED
-  (___, ___, ___, ___, ___, ___, ___,
-   ___, ___, ___, ___, ___, ___, ___,
-   ___, ___, ___, ___, ___, ___,
-   ___, ___, ___, ___, ___, ___, ___,
-   ___, ___, ___, ___,
-   ___,
-
-   M(MACRO_VERSION_INFO),  ___, Key_Keypad7, Key_Keypad8,   Key_Keypad9,        Key_KeypadSubtract, ___,
-   ___,                    ___, Key_Keypad4, Key_Keypad5,   Key_Keypad6,        Key_KeypadAdd,      ___,
-                           ___, Key_Keypad1, Key_Keypad2,   Key_Keypad3,        Key_Equals,         ___,
-   ___,                    ___, Key_Keypad0, Key_KeypadDot, Key_KeypadMultiply, Key_KeypadDivide,   Key_Enter,
-   ___, ___, ___, ___,
    ___)
-
 	) // KEYMAPS(
 
 /* Re-enable astyle's indent enforcement */
@@ -346,7 +346,7 @@ void setup() {
     // The MouseKeys plugin lets you add keys to your keymap which move the mouse.
     &MouseKeys,
 
-    &Qukeys
+    &Qukeys,
     // The HostPowerManagement plugin enables waking up the host from suspend,
     // and allows us to turn LEDs off when it goes to sleep.
     &HostPowerManagement
